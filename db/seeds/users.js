f014d748ff44d42b2cb5fafdf1fa7cd6a0ffa19f
Anthony Morris <anthony@amorrissound.com>
2016-09-28 20:23:31 +0000

    Add migration for users table creation and seed data

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,9 +2,11 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('users').insert({id: 1, username: 'bigjohn77'}),
+        knex('users').insert({id: 2, username: 'fierytomato'}),
+        knex('users').insert({id: 3, username: 'l337hax0r'})
+        knex('users').insert({id: 4, username: 'ErikWeisz13'})
+        knex('users').insert({id: 5, username: 'Stan'})
       ]);
     });
 };
