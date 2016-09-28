3f150117eae2827eb17dfd8e73d76f6041566d36
Anthony Morris <anthony@amorrissound.com>
2016-09-28 22:34:23 +0000

    Setup route for getting a user profile page

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,10 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
-  router.get("/", (req, res) => {
-    knex
-      .select("*")
-      .from("users")
-      .then((results) => {
-        res.json(results);
-    });
+  router.get("/:id", (req, res) => {
+    // TO-DO: Implement data from db for users
+    let templateVars = { username: 'bigjohn77' };
+    res.render("user_profile", templateVars);
   });
 
   return router;
