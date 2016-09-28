0649c83e523e7f7486f50ba579fab2e2adbf6984
Anthony Morris <anthony@amorrissound.com>
2016-09-28 20:30:17 +0000

    Fix syntax issue in users.js seed

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -4,8 +4,8 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         knex('users').insert({id: 1, username: 'bigjohn77'}),
         knex('users').insert({id: 2, username: 'fierytomato'}),
-        knex('users').insert({id: 3, username: 'l337hax0r'})
-        knex('users').insert({id: 4, username: 'ErikWeisz13'})
+        knex('users').insert({id: 3, username: 'l337hax0r'}),
+        knex('users').insert({id: 4, username: 'ErikWeisz13'}),
         knex('users').insert({id: 5, username: 'Stan'})
       ]);
     });
