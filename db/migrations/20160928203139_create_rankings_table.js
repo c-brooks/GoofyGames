9779e454fdda2e4dd7b984a6347b09d7486d489b
Anthony Morris <anthony@amorrissound.com>
2016-09-28 21:39:47 +0000

    Fix issues in rankings table

--- db/migrations/20160928203139_create_rankings_table.js
+++ db/migrations/20160928203139_create_rankings_table.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('rankings', function (table) {
     table.increments();
     table.integer('game_id');
-    tabke.integer('player_id');
+    table.integer('player_id');
     table.integer('wins');
   });
 };
