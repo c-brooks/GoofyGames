63ecb8c1db6fbfe3a197f93d44ea072b499120dd
Anthony Morris <anthony@amorrissound.com>
2016-09-29 01:50:23 +0000

    Add losses to rankings and setup rankings seed data

--- db/seeds/rankings.js
+++ db/seeds/rankings.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('rankings').insert({id: 1, game_id: 1, player_id: 1, wins: 666}),
-        knex('rankings').insert({id: 2, game_id: 1, player_id: 2, wins: 2}),
-        knex('rankings').insert({id: 3, game_id: 1, player_id: 4, wins: 1})
+        knex('rankings').insert({id: 1, game_id: 1, player_id: 1, wins: 666, losses: 3}),
+        knex('rankings').insert({id: 2, game_id: 1, player_id: 2, wins: 2, losses: 0}),
+        knex('rankings').insert({id: 3, game_id: 1, player_id: 4, wins: 1, losses: 2334})
       ]);
     });
 };
