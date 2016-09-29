78ea31068621768b77021e8d25e40cd3427ba745
Anthony Morris <anthony@amorrissound.com>
2016-09-29 00:51:11 +0000

    Update index.ejs file

--- routes/users.js
+++ routes/users.js
@@ -9,9 +9,9 @@ module.exports = (knex) => {
 
   router.get("/:id", (req, res) => {
     usersRepo.getUser(req.params.id, function(user) {
-      // archivedMatchesRepo.getArchivedMatches(req.params.id, function(archived_matches) {
-      //   var archived_matches = archived_matches;
-      // })
+      archivedMatchesRepo.getArchivedMatches(req.params.id, function(archived_matches) {
+        var archived_matches = archived_matches;
+      })
       let templateVars = { title: user.username, user: user, archived_matches: { one: 1, two: 2 } };
       res.render("profile", templateVars);
     });
