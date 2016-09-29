d7e5e544fb5c4e7ca3c33bb77e51982ae460c4e1
Anthony Morris <anthony@amorrissound.com>
2016-09-29 00:04:59 +0000

    Added archived_matches to user profile route

--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,15 @@ const router  = express.Router();
 
 module.exports = (knex) => {
   const usersRepo = require('../db/users.js')(knex);
+  const archivedMatchesRepo = require('../db/archived_matches.js')(knex);
 
   router.get("/:id", (req, res) => {
     usersRepo.getUser(req.params.id, function(user) {
-      let templateVars = { user: user };
+      archivedMatchesRepo.getArchivedMatches(req.params.id, function(archived_matches) {
+        var archived_matches = archived_matches;
+      })
+
+      let templateVars = { user: user, archived_matches: archived_matches };
       // res.render("user_profile", templateVars);
     });
   });
