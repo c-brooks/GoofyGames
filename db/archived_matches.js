a8cdb4770415e6d679587609a39211ec58b6c3d0
Anthony Morris <anthony@amorrissound.com>
2016-09-29 01:40:29 +0000

    Get names of users involved in archived matches

--- db/archived_matches.js
+++ db/archived_matches.js
@@ -3,8 +3,10 @@ const archivedMatchesRepo = {};
 module.exports = (knex) => {
   archivedMatchesRepo.getArchivedMatches = (user_id, cb) => {
     knex
-    .select('*')
+    .select('archived_matches.*', 'winner.username AS winner_name', 'loser.username as loser_name')
     .from('archived_matches')
+    .leftJoin('users AS winner', 'winner.id', 'archived_matches.winner_id')
+    .leftJoin('users AS loser', 'loser.id', 'archived_matches.loser_id')
     .where('winner_id', '=', user_id)
     .orWhere('loser_id', '=', user_id)
     .asCallback((err, archived_matches) => {
