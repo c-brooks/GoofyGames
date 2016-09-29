945b956e4d8b23a4f3b93dd78a48eae9aa3bc628
Anthony Morris <anthony@amorrissound.com>
2016-09-29 00:43:10 +0000

    Fix issues with partial files

--- routes/users.js
+++ routes/users.js
@@ -9,12 +9,11 @@ module.exports = (knex) => {
 
   router.get("/:id", (req, res) => {
     usersRepo.getUser(req.params.id, function(user) {
-      archivedMatchesRepo.getArchivedMatches(req.params.id, function(archived_matches) {
-        var archived_matches = archived_matches;
-      })
-
-      let templateVars = { user: user, archived_matches: archived_matches };
-      // res.render("user_profile", templateVars);
+      // archivedMatchesRepo.getArchivedMatches(req.params.id, function(archived_matches) {
+      //   var archived_matches = archived_matches;
+      // })
+      let templateVars = { title: user.username, user: user, archived_matches: { one: 1, two: 2 } };
+      res.render("profile", templateVars);
     });
   });
 
