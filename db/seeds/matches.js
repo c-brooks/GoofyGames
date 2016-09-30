d4417028263b017af8a903b4e3f4d19ff9fa3b51
Anthony Morris <anthony@amorrissound.com>
2016-09-30 20:32:07 +0000

    Change seed data to match new form for storing cards { suit: 'spade', value: '7' }

--- db/seeds/matches.js
+++ db/seeds/matches.js
@@ -18,24 +18,64 @@ exports.seed = function(knex, Promise) {
           whose_move: 1,
           deck_cards:
           {
-            spades:   [6,7,8,9,10,11,13],
-            hearts:   [1,2,3,4,5,11,12],
-            clubs:    [6,7,8,9,10,11,13],
-            diamonds: [1,2,3,4,5,11,12]
+            { suit: 'heart', value: '1' },
+            { suit: 'heart', value: '2' },
+            { suit: 'heart', value: '3' },
+            { suit: 'heart', value: '4' },
+            { suit: 'heart', value: '5' },
+            { suit: 'heart', value: '6' },
+            { suit: 'heart', value: '7' },
+            { suit: 'heart', value: '8' },
+            { suit: 'heart', value: '9' },
+            { suit: 'heart', value: '10' },
+            { suit: 'heart', value: '11' },
+            { suit: 'heart', value: '12' },
+            { suit: 'heart', value: '13' },
+            { suit: 'club', value: '1' },
+            { suit: 'club', value: '2' },
+            { suit: 'club', value: '3' },
+            { suit: 'club', value: '4' },
+            { suit: 'club', value: '5' },
+            { suit: 'club', value: '6' },
+            { suit: 'club', value: '7' },
+            { suit: 'club', value: '8' },
+            { suit: 'club', value: '9' },
+            { suit: 'club', value: '10' },
+            { suit: 'club', value: '11' },
+            { suit: 'club', value: '12' },
+            { suit: 'club', value: '13' }
           },
           player1_cards:
           {
-            spades: [1,2,3,4,5],
-            hearts: [6,7,8,9,10],
-            clubs: [12],
-            diamonds: [13]
+            { suit: 'spade', value: '1' },
+            { suit: 'spade', value: '2' },
+            { suit: 'spade', value: '3' },
+            { suit: 'spade', value: '4' },
+            { suit: 'spade', value: '5' },
+            { suit: 'spade', value: '6' },
+            { suit: 'spade', value: '7' },
+            { suit: 'spade', value: '8' },
+            { suit: 'spade', value: '9' },
+            { suit: 'spade', value: '10' },
+            { suit: 'spade', value: '11' },
+            { suit: 'spade', value: '12' },
+            { suit: 'spade', value: '13' }
           },
           player2_cards:
           {
-            spades: [12],
-            hearts: [13],
-            clubs: [1,2,3,4,5],
-            diamonds: [6,7,8,9,10]
+            { suit: 'diamond', value: '1' },
+            { suit: 'diamond', value: '2' },
+            { suit: 'diamond', value: '3' },
+            { suit: 'diamond', value: '4' },
+            { suit: 'diamond', value: '5' },
+            { suit: 'diamond', value: '6' },
+            { suit: 'diamond', value: '7' },
+            { suit: 'diamond', value: '8' },
+            { suit: 'diamond', value: '9' },
+            { suit: 'diamond', value: '10' },
+            { suit: 'diamond', value: '11' },
+            { suit: 'diamond', value: '12' },
+            { suit: 'diamond', value: '13' }
           },
           game_start: '1999-01-08 04:05:06'
         })
