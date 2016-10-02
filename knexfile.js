1b95d96520b8a5866854a21d5da5dcf47b84df90
Anthony Morris <anthony@amorrissound.com>
2016-10-02 22:23:52 +0000

    Removed pools

--- knexfile.js
+++ knexfile.js
@@ -24,10 +24,6 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL + '?ssl=true',
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       tableName: 'migrations'
     }
