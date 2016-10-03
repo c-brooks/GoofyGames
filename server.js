8baef6c894e6e943821a03967c69a6a3b97f8730
Anthony Morris <anthony@amorrissound.com>
2016-10-03 17:24:51 +0000

    Push to Heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 "use strict";
 
-require('dotenv').config();
+// require('dotenv').config();
 
 const PORT          = process.env.PORT || 8080;
 const ENV           = process.env.ENV || "development";
